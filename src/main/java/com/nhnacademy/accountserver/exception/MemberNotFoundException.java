5fdd5f97b74071f11c9a4faf5cf6fb484980f920
kkkkimtaehyeon <kth1927@naver.com>
2024-11-28 16:13:02 +0900

    예외처리 구현

--- src/main/java/com/nhnacademy/accountserver/exception/MemberNotFoundException.java
+++ src/main/java/com/nhnacademy/accountserver/exception/MemberNotFoundException.java
@@ -1,7 +1,7 @@
 package com.nhnacademy.accountserver.exception;
 
 public class MemberNotFoundException extends RuntimeException {
-    public MemberNotFoundException(String message) {
-        super(message);
+    public MemberNotFoundException() {
+        super("등록되지 않은 회원입니다.");
     }
 }
